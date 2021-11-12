e31eb1ae8fa4be930f76ca77ba0f609bf6feb199
Thiago Vieira <th91vi@gmail.com>
2021-11-12 15:03:20 -0300

    refactor: added TextInput to Editor component

--- src/components/structure/Editor/Editor.jsx
+++ src/components/structure/Editor/Editor.jsx
@@ -1,3 +1,66 @@
+import { TextInput } from "../../form/TextField";
+
 export function Editor() {
-  return <div>Editor</div>;
+  return (
+    <div>
+      <h2>Editor</h2>
+      <TextInput
+        fullWidth={true}
+        margin="1"
+        label="SKU"
+        placeholder="Exemplo: {{.gtin}}"
+      />
+
+      <TextInput
+        fullWidth={true}
+        margin="1"
+        label="Título"
+        placeholder="Exemplo: {{.title}}"
+      />
+
+      <TextInput
+        fullWidth={true}
+        margin="1"
+        label="Descrição"
+        placeholder="Exemplo: {{.description}}"
+      />
+
+      <TextInput
+        fullWidth={true}
+        margin="1"
+        label="Preço"
+        placeholder="Exemplo: {{.price}}"
+      />
+
+      <TextInput
+        fullWidth={true}
+        margin="1"
+        label="Link"
+        placeholder="Exemplo: {{.link}}"
+      />
+
+      <TextInput
+        fullWidth={true}
+        margin="1"
+        label="Imagem"
+        placeholder="Exemplo: {{.image_link}}"
+      />
+
+      <TextInput
+        fullWidth={true}
+        margin="1"
+        label="Fabricante"
+        placeholder="Exemplo: {{.brand}}"
+      />
+
+      <TextInput fullWidth={true} margin="1" label="Ingrediente ativo" />
+
+      <TextInput
+        fullWidth={true}
+        margin="1"
+        label="Disponibilidade"
+        placeholder='{{eq .availability \"out of stock\"}}'
+      />
+    </div>
+  );
 }
