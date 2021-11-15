e2ed3caed5f93e0b0b09fc87c70d43b60a6442b8
Thiago Vieira <th91vi@gmail.com>
2021-11-15 19:34:09 -0300

    feat: added logic to search properties in mocked data and add it to preview list

--- src/components/structure/Preview/Preview.jsx
+++ src/components/structure/Preview/Preview.jsx
@@ -1,3 +1,59 @@
+import { useState } from "react";
+import mockData from "../../../../mock/4bio.json";
+
 export function Preview() {
-  return <div>Preview</div>;
+  const [productsList, setProductsList] = useState([]);
+
+  const items = mockData.channel.item;
+
+  const countSkus = (skuValue) => {
+    if (!items[0].hasOwnProperty(skuValue)) return;
+
+    const foundSkus = items.map((item) => ({
+      sku: item[skuValue],
+    }));
+
+    setProductsList(foundSkus);
+  };
+
+  const findProperty = (property) => {
+    if (!items[0].hasOwnProperty(property)) return;
+
+    const newProductsList = productsList.map((product, index) => {
+      if (!product.hasOwnProperty(property)) {
+        const newSku = { ...product, [property]: items[index][property] };
+
+        return newSku;
+      }
+    });
+
+    setProductsList(newProductsList);
+  };
+
+  const handleSearch = (property) => {
+    if (!productsList.length) {
+      countSkus(property);
+    } else {
+      findProperty(property);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Preview</h2>
+      <input type="text" onChange={(e) => handleSearch(e.target.value)} />
+      <ul>
+        {productsList.map((item, index) => (
+          <li key={index}>
+            {Object.entries(item).map((entry, index) => (
+              <pre key={index}>
+                <strong>{`${entry[0]}: `}</strong>
+                {`${entry[1]}`}
+              </pre>
+            ))}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
